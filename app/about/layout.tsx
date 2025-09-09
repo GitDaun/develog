64f6795323f678c7eafbdeba810cfb0181c2427c
Daun <daunje0@gmail.com>
2025-09-10 03:04:20 +0900

    tailwind Typography 플러그인 적용

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,7 +1,15 @@
+import localFont from "next/font/local";
+
+const ongeulypKonkon = localFont({
+  src: '../fonts/ongeulyp_konkon.ttf',
+  display: 'swap',
+  weight: '400',
+});
+
 export default function AboutLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      <div>{children}</div>
+    <div  className={ongeulypKonkon.className}>
+      <div className="prose prose-invert">{children}</div>
       <div className="mt-8">
         <h2 className="text-xl mb-4">About 레이아웃입니다</h2>
         <ul>
